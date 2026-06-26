6e3cf201b03468462f3ebe960dcf20c52d400356
Diptiprakash <dpsray.jira@gmail.com>
2026-06-26 21:29:06 +0530

    added

--- typescript-training/language-basics/loop-statements.ts
+++ typescript-training/language-basics/loop-statements.ts
@@ -1,40 +1,119 @@
-let name : string ="Krishu";
+//Loop Statements in TypeScript => Loop statements are nothing but the conditions that we are going to add along with the statements to execute the same code multiple times. 
 
-console.log(name);
+let name : string = "Aravind";
 
-//for loop
-for(let i :number=1; i<=10; i++){
+
+//Before Loops
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+
+//Loop statements are mainly divided into two different categories. 
+
+//1. for loop => When we know the total number of iterations to be executed before itself 
+//2. while loop => When we are not sure about the total number of iterations to be executed 
+
+//1. for loop 
+//Syntax => for(condition-to-start; condition-to-end; interval) { 
+// code to be executed 
+// }
+
+for(let i :number = 1;i<=10 ;i++){
     console.log(name);
 }
 
 
+//2. while loop
+//Syntax : while(condition-to-start){
+//  code to execute
+//}
 
-//while loop
-let j :number=1;
-let isPageisloaded : boolean=false;
+let j : number = 1;
+let isPageLoaded : boolean = false;
 
-while(j>0){
+while(j>0){    
+    if(j>5 || isPageLoaded){
+        break; //stop the execution and come out of the loop
+    }
+    console.log("Refresh the page. ")
 
-    if(j>5 || isPageisloaded){
-    break;}
-    console.log("Refresh the page")
-    j++ ;
+    j++; //Increment the value of j by 1
 }
 
-//special cases in loop
-//for --of loop
-let fruits: string[] =["mango","orange","banana","coconum"]
 
-for(let i:number=0;i<fruits.length;i++)
-{
+//Special cases in Loops
+
+//1. for..of loop => Used to iterate over the values of an array 
+//2. for..in loop => Use to iterate over the properties of an object. 
+//3. do..while loop => Use to execute the block of code at least once, even when the condition is not satisfied. 
+
+
+//Array
+let fruits : string []= ["Apple", "Banana", "Mango", "Grapes"];
+
+//normal for loop
+for(let i:number =0 ; i<fruits.length ;i++){
     console.log(fruits[i]);
 }
 
+//1. for..of loop => Iterate over each and every value of the given list. 
+//Syntax : for(let value of list){
+//  code to execute
+//}
 
-for(let fruit of fruits){   // it will print all the values
-    console.log(fruit)
+for(let fruit of fruits){
+    console.log(fruit);
 }
 
+//2. for..in loop => Iterate over each and every property of the given object.
+//Syntax : for(let property in object){
+//  code to execute
+//}
 
-//for in loop
-for
+//create template to store object
+interface empInfo {
+    "empName": string,
+    "empId": number,
+    "visaStatus": boolean,
+    "address": {
+        "city": string,
+        "state": string
+    }
+}
+
+//storing the data
+let empDetails : empInfo = {
+    "empName": "Bharath",
+    "empId": 1234,
+    "visaStatus": true,
+    "address": {
+        "city": "Hyderabad",
+        "state": "Telangana"
+    }
+}
+
+for(let key in empDetails){
+    console.log(key);
+    console.log(empDetails[key as keyof empInfo]);
+}
+
+//do..while loop => Execute the block of code at least once, even when the condition is not satisfied.
+
+//Syntax : do{
+//  code to execute
+//}while(condition-to-start)
+
+console.log("do..while loop example");
+let x : number = 0;
+do{
+    console.log("Refresh the Page");
+    x++;
+}while(x > 0);
